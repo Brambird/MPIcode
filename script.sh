384c502563ce2f07b79389248958959cac6467d1
Bram Vogels <bvogels@cn47.science.ru.nl>
2021-05-26 21:50:51 +0200

    Finalized.

--- script.sh
+++ script.sh
@@ -1,28 +1,193 @@
 #!/usr/bin/env bash
 
 #SBATCH -p csedu
+#SBATCH -n 16
 
-mpicc -DMPI -O3 -march=native -o prog relax-mpi.c
-mpirun --mca btl_base_warn_component_unused 0 -n 1 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 1 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 1 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 1 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 1 ./prog 5000 10
+mpicc -DMPI -O3 -march=native -o prog relax-newz.c -lm
 
-mpirun --mca btl_base_warn_component_unused 0 -n 2 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 2 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 2 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 2 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 2 ./prog 5000 10
+#1 ranks
+mpirun -n 1 ./prog 500 10
+mpirun -n 1 ./prog 500 10
+mpirun -n 1 ./prog 500 10
+mpirun -n 1 ./prog 500 10
+mpirun -n 1 ./prog 500 10
 
-mpirun --mca btl_base_warn_component_unused 0 -n 4 ./prog 5000 10 
-mpirun --mca btl_base_warn_component_unused 0 -n 4 ./prog 5000 10 
-mpirun --mca btl_base_warn_component_unused 0 -n 4 ./prog 5000 10  
-mpirun --mca btl_base_warn_component_unused 0 -n 4 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 4 ./prog 5000 10
+mpirun -n 1 ./prog 1000 10
+mpirun -n 1 ./prog 1000 10
+mpirun -n 1 ./prog 1000 10
+mpirun -n 1 ./prog 1000 10
+mpirun -n 1 ./prog 1000 10
 
-mpirun --mca btl_base_warn_component_unused 0 -n 8 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 8 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 8 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 8 ./prog 5000 10
-mpirun --mca btl_base_warn_component_unused 0 -n 8 ./prog 5000 10
+mpirun -n 1 ./prog 5000 10
+mpirun -n 1 ./prog 5000 10
+mpirun -n 1 ./prog 5000 10
+mpirun -n 1 ./prog 5000 10
+mpirun -n 1 ./prog 5000 10
+
+mpirun -n 1 ./prog 10000 10
+mpirun -n 1 ./prog 10000 10
+mpirun -n 1 ./prog 10000 10
+mpirun -n 1 ./prog 10000 10
+mpirun -n 1 ./prog 10000 10
+
+mpirun -n 1 ./prog 15000 10
+mpirun -n 1 ./prog 15000 10
+mpirun -n 1 ./prog 15000 10
+mpirun -n 1 ./prog 15000 10
+mpirun -n 1 ./prog 15000 10
+
+mpirun -n 1 ./prog 25000 10
+mpirun -n 1 ./prog 25000 10
+mpirun -n 1 ./prog 25000 10
+mpirun -n 1 ./prog 25000 10
+mpirun -n 1 ./prog 25000 10
+
+#2 ranks
+mpirun -n 2 ./prog 500 10
+mpirun -n 2 ./prog 500 10
+mpirun -n 2 ./prog 500 10
+mpirun -n 2 ./prog 500 10
+mpirun -n 2 ./prog 500 10
+
+mpirun -n 2 ./prog 1000 10
+mpirun -n 2 ./prog 1000 10
+mpirun -n 2 ./prog 1000 10
+mpirun -n 2 ./prog 1000 10
+mpirun -n 2 ./prog 1000 10
+
+mpirun -n 2 ./prog 5000 10
+mpirun -n 2 ./prog 5000 10
+mpirun -n 2 ./prog 5000 10
+mpirun -n 2 ./prog 5000 10
+mpirun -n 2 ./prog 5000 10
+
+mpirun -n 2 ./prog 10000 10
+mpirun -n 2 ./prog 10000 10
+mpirun -n 2 ./prog 10000 10
+mpirun -n 2 ./prog 10000 10
+mpirun -n 2 ./prog 10000 10
+
+mpirun -n 2 ./prog 15000 10
+mpirun -n 2 ./prog 15000 10
+mpirun -n 2 ./prog 15000 10
+mpirun -n 2 ./prog 15000 10
+mpirun -n 2 ./prog 15000 10
+
+mpirun -n 2 ./prog 25000 10
+mpirun -n 2 ./prog 25000 10
+mpirun -n 2 ./prog 25000 10
+mpirun -n 2 ./prog 25000 10
+mpirun -n 2 ./prog 25000 10
+
+
+#4 ranks
+mpirun -n 4 ./prog 500 10
+mpirun -n 4 ./prog 500 10
+mpirun -n 4 ./prog 500 10
+mpirun -n 4 ./prog 500 10
+mpirun -n 4 ./prog 500 10
+
+mpirun -n 4 ./prog 1000 10
+mpirun -n 4 ./prog 1000 10
+mpirun -n 4 ./prog 1000 10
+mpirun -n 4 ./prog 1000 10
+mpirun -n 4 ./prog 1000 10
+
+mpirun -n 4 ./prog 5000 10
+mpirun -n 4 ./prog 5000 10
+mpirun -n 4 ./prog 5000 10
+mpirun -n 4 ./prog 5000 10
+mpirun -n 4 ./prog 5000 10
+
+mpirun -n 4 ./prog 10000 10
+mpirun -n 4 ./prog 10000 10
+mpirun -n 4 ./prog 10000 10
+mpirun -n 4 ./prog 10000 10
+mpirun -n 4 ./prog 10000 10
+
+mpirun -n 4 ./prog 15000 10
+mpirun -n 4 ./prog 15000 10
+mpirun -n 4 ./prog 15000 10
+mpirun -n 4 ./prog 15000 10
+mpirun -n 4 ./prog 15000 10
+
+mpirun -n 4 ./prog 25000 10
+mpirun -n 4 ./prog 25000 10
+mpirun -n 4 ./prog 25000 10
+mpirun -n 4 ./prog 25000 10
+mpirun -n 4 ./prog 25000 10
+
+#8 ranks
+mpirun -n 8 ./prog 500 10
+mpirun -n 8 ./prog 500 10
+mpirun -n 8 ./prog 500 10
+mpirun -n 8 ./prog 500 10
+mpirun -n 8 ./prog 500 10
+
+mpirun -n 8 ./prog 1000 10
+mpirun -n 8 ./prog 1000 10
+mpirun -n 8 ./prog 1000 10
+mpirun -n 8 ./prog 1000 10
+mpirun -n 8 ./prog 1000 10
+
+mpirun -n 8 ./prog 5000 10
+mpirun -n 8 ./prog 5000 10
+mpirun -n 8 ./prog 5000 10
+mpirun -n 8 ./prog 5000 10
+mpirun -n 8 ./prog 5000 10
+
+mpirun -n 8 ./prog 10000 10
+mpirun -n 8 ./prog 10000 10
+mpirun -n 8 ./prog 10000 10
+mpirun -n 8 ./prog 10000 10
+mpirun -n 8 ./prog 10000 10
+
+mpirun -n 8 ./prog 15000 10
+mpirun -n 8 ./prog 15000 10
+mpirun -n 8 ./prog 15000 10
+mpirun -n 8 ./prog 15000 10
+mpirun -n 8 ./prog 15000 10
+
+mpirun -n 8 ./prog 25000 10
+mpirun -n 8 ./prog 25000 10
+mpirun -n 8 ./prog 25000 10
+mpirun -n 8 ./prog 25000 10
+mpirun -n 8 ./prog 25000 10
+
+
+#16 ranks
+mpirun -n 16 ./prog 500 10
+mpirun -n 16 ./prog 500 10
+mpirun -n 16 ./prog 500 10
+mpirun -n 16 ./prog 500 10
+mpirun -n 16 ./prog 500 10
+
+mpirun -n 16 ./prog 1000 10
+mpirun -n 16 ./prog 1000 10
+mpirun -n 16 ./prog 1000 10
+mpirun -n 16 ./prog 1000 10
+mpirun -n 16 ./prog 1000 10
+
+mpirun -n 16 ./prog 5000 10
+mpirun -n 16 ./prog 5000 10
+mpirun -n 16 ./prog 5000 10
+mpirun -n 16 ./prog 5000 10
+mpirun -n 16 ./prog 5000 10
+
+mpirun -n 16 ./prog 10000 10
+mpirun -n 16 ./prog 10000 10
+mpirun -n 16 ./prog 10000 10
+mpirun -n 16 ./prog 10000 10
+mpirun -n 16 ./prog 10000 10
+
+mpirun -n 16 ./prog 15000 10
+mpirun -n 16 ./prog 15000 10
+mpirun -n 16 ./prog 15000 10
+mpirun -n 16 ./prog 15000 10
+mpirun -n 16 ./prog 15000 10
+
+mpirun -n 16 ./prog 25000 10
+mpirun -n 16 ./prog 25000 10
+mpirun -n 16 ./prog 25000 10
+mpirun -n 16 ./prog 25000 10
+mpirun -n 16 ./prog 25000 10
